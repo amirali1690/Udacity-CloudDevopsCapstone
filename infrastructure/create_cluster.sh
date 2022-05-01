7c2c2b78cde95ef25e1240a32913576cbf09c06b
Ubuntu <ubuntu@ip-172-31-8-205.ec2.internal>
2022-05-01 16:28:45 +0000

    changed the node-type to medium

--- infrastructure/create_cluster.sh
+++ infrastructure/create_cluster.sh
@@ -1,1 +1,2 @@
-eksctl create cluster --name amirali1690-Capstone --version 1.21 --nodegroup-name capstone-nodes --node-type t3.nano --nodes 3 --nodes-min 1 --nodes-max 4 --region us-east-1
+eksctl create cluster --name amirali1690-capstone --version 1.21 --nodegroup-name main-nodes --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 4 --region us-east-1
+
