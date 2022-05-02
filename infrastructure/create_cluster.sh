a48646f4b668b0e63e38f67d84362f175054db74
Ubuntu <ubuntu@ip-172-31-8-205.ec2.internal>
2022-05-02 01:17:30 +0000

    change config of the nodes

--- infrastructure/create_cluster.sh
+++ infrastructure/create_cluster.sh
@@ -1,2 +1,9 @@
-eksctl create cluster --name amirali1690-capstone --version 1.21 --nodegroup-name main-nodes --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 4 --region us-east-1
+eksctl create cluster \
+--name amirali1690-capstone-cluster \
+--version 1.22 \
+--region us-east-1 \
+--zones us-east-1a,us-east-1b \
+--nodegroup-name capstone-nodes \
+--node-type t3.medium \
+--nodes 2
 
